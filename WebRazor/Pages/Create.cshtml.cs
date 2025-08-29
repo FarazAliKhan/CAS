77ee0f017044869f60261f16a1adb223ecb0b098
Abul Farhan Siddiqui <Frz.khan1@gmail.com>
2025-08-28 22:37:34 -0400

    validations

--- WebRazor/Pages/Create.cshtml.cs
+++ WebRazor/Pages/Create.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 using SmartBreadcrumbs.Attributes;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using WebRazor.Models;
 
@@ -12,14 +13,19 @@ namespace WebRazor.Pages
     public class CreateModel : PageModel
     {
         [BindProperty]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "First Name should be atleast 2 characters long")]
         public string firstName { get; set; }
         [BindProperty]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Last Name should be atleast 2 characters long")]
         public string lastName { get; set; }
         [BindProperty]
         public DateTime dob { get; set; }
-        [BindProperty]
-        public int Id { get; set; }
 
         public CreateModel() { }
+
+        public void OnPost() {
+            if (!ModelState.IsValid)
+                return;
+        }
     }
 }
