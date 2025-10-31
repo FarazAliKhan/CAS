e997c3cc5203beac3c6fbe1f84ca20f4031ffb2a
FarazAliKhan <Frz.khan1@gmail.com>
2025-10-31 09:22:33 -0400

    Make section required on coming from review

--- WebRazor/wwwroot/js/site.js
+++ WebRazor/wwwroot/js/site.js
@@ -44,66 +44,77 @@ function fieldFocus(sectionId) {
             $('#details1').removeAttr('hidden');
             document.getElementById("txtFIELD1_1_1").focus();
             document.getElementById("txtFIELD1_1_1").setAttribute("required", "true");
+            makeSection1Required();
             break;
         case "2": 
             $("#lidetails2").css("background", "#1C578A");
             $("#activeDetailsId").val("details2");
             $('#details2').removeAttr('hidden');
             document.getElementById("txtFIELD2_1_1").focus();
+            makeSection2Required();
             break;
         case "3": 
             $("#lidetails3").css("background", "#1C578A");
             $("#activeDetailsId").val("details3");
             $('#details3').removeAttr('hidden');
             document.getElementById("txtFIELD3_1").focus();
+            makeSection3Required();
             break;
         case "4": 
             $("#lidetails4").css("background", "#1C578A");
             $("#activeDetailsId").val("details4");
             $('#details4').removeAttr('hidden');
             document.getElementById("txtFIELD4_1_1").focus();
+            makeSection4Required();
             break;
         case "5": 
             $("#lidetails5").css("background", "#1C578A");
             $("#activeDetailsId").val("details5");
             $('#details5').removeAttr('hidden');
             document.getElementById("txtFIELD5_1").focus();
+            makeSection5Required();
             break;
         case "6": 
             $("#lidetails6").css("background", "#1C578A");
             $("#activeDetailsId").val("details6");
             $('#details6').removeAttr('hidden');
             document.getElementById("txtFIELD6_1_1").focus();
+            makeSection6Required();
             break;
         case "7": 
             $("#lidetails7").css("background", "#1C578A");
             $("#activeDetailsId").val("details7");
             $('#details7').removeAttr('hidden');
             document.getElementById("txtFIELD7_1_1").focus();
+            makeSection7Required();
             break;
         case "8": 
             $("#lidetails8").css("background", "#1C578A");
             $("#activeDetailsId").val("details8");
             $('#details8').removeAttr('hidden');
             document.getElementById("txtFIELD8_1").focus();
+            makeSection8Required();
             break;
         case "9": 
             $("#lidetails9").css("background", "#1C578A");
             $("#activeDetailsId").val("details9");
             $('#details9').removeAttr('hidden');
             document.getElementById("txtFIELD9_1").focus();
+            makeSection9Required();
             break;
         case "10": 
             $("#lidetails10").css("background", "#1C578A");
             $("#activeDetailsId").val("details10");
             $('#details10').removeAttr('hidden');
             document.getElementById("txtFIELD10_1").focus();
+            makeSection10Required();
             break;
         case "11": 
             $("#lidetails11").css("background", "#1C578A");
             $("#activeDetailsId").val("details11");
             $('#details11').removeAttr('hidden');
             document.getElementById("txtFIELD11_1").focus();
+            makeSection11Required();
             break;
         case "12":
             $("#lidetails12").css("background", "#1C578A");
