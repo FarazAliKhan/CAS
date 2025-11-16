5cc8709a96b2fb5f11a51f463c679ea61aeb03d1
FarazAliKhan <Frz.khan1@gmail.com>
2025-11-16 15:42:33 -0500

    info popups

--- WebRazor/wwwroot/js/site.js
+++ WebRazor/wwwroot/js/site.js
@@ -38,7 +38,7 @@ function focusOnForm(sectionId) {
 
 function fieldFocus(sectionId) {
     switch (sectionId) {
-        case "1": 
+        case "1":
             $("#lidetails1").css("background", "#1C578A");
             $("#activeDetailsId").val("details1");
             $('#details1').removeAttr('hidden');
@@ -46,70 +46,70 @@ function fieldFocus(sectionId) {
             document.getElementById("txtFIELD1_1_1").setAttribute("required", "true");
             makeSection1Required();
             break;
-        case "2": 
+        case "2":
             $("#lidetails2").css("background", "#1C578A");
             $("#activeDetailsId").val("details2");
             $('#details2').removeAttr('hidden');
             document.getElementById("txtFIELD2_1_1").focus();
             makeSection2Required();
             break;
-        case "3": 
+        case "3":
             $("#lidetails3").css("background", "#1C578A");
             $("#activeDetailsId").val("details3");
             $('#details3').removeAttr('hidden');
             document.getElementById("txtFIELD3_1").focus();
             makeSection3Required();
             break;
-        case "4": 
+        case "4":
             $("#lidetails4").css("background", "#1C578A");
             $("#activeDetailsId").val("details4");
             $('#details4').removeAttr('hidden');
             document.getElementById("txtFIELD4_1_1").focus();
             makeSection4Required();
             break;
-        case "5": 
+        case "5":
             $("#lidetails5").css("background", "#1C578A");
             $("#activeDetailsId").val("details5");
             $('#details5').removeAttr('hidden');
             document.getElementById("txtFIELD5_1").focus();
             makeSection5Required();
             break;
-        case "6": 
+        case "6":
             $("#lidetails6").css("background", "#1C578A");
             $("#activeDetailsId").val("details6");
             $('#details6').removeAttr('hidden');
             document.getElementById("txtFIELD6_1_1").focus();
             makeSection6Required();
             break;
-        case "7": 
+        case "7":
             $("#lidetails7").css("background", "#1C578A");
             $("#activeDetailsId").val("details7");
             $('#details7').removeAttr('hidden');
             document.getElementById("txtFIELD7_1_1").focus();
             makeSection7Required();
             break;
-        case "8": 
+        case "8":
             $("#lidetails8").css("background", "#1C578A");
             $("#activeDetailsId").val("details8");
             $('#details8').removeAttr('hidden');
             document.getElementById("txtFIELD8_1").focus();
             makeSection8Required();
             break;
-        case "9": 
+        case "9":
             $("#lidetails9").css("background", "#1C578A");
             $("#activeDetailsId").val("details9");
             $('#details9').removeAttr('hidden');
             document.getElementById("txtFIELD9_1").focus();
             makeSection9Required();
             break;
-        case "10": 
+        case "10":
             $("#lidetails10").css("background", "#1C578A");
             $("#activeDetailsId").val("details10");
             $('#details10').removeAttr('hidden');
             document.getElementById("txtFIELD10_1").focus();
             makeSection10Required();
             break;
-        case "11": 
+        case "11":
             $("#lidetails11").css("background", "#1C578A");
             $("#activeDetailsId").val("details11");
             $('#details11').removeAttr('hidden');
@@ -122,7 +122,7 @@ function fieldFocus(sectionId) {
             $('#details12').removeAttr('hidden');
             document.getElementById("txtFIED_12_Comments").focus();
             break;
-        case '0': 
+        case '0':
             $("#lidetailsBasicInfo").css("background", "#1C578A");
             $("#activeDetailsId").val("detailsBasicInfo");
             $('#detailsBasicInfo').removeAttr('hidden');
@@ -188,7 +188,7 @@ function hideDetails(detailsId) {
 function gotoPrevious() {
     let activeDetails = $("#activeDetailsId").val();
     switch (activeDetails) {
-        case "detailsBasicInfo": 
+        case "detailsBasicInfo":
             break;
         case "details1": showDetails("detailsBasicInfo");
             $("#lidetailsBasicInfo").css("background", "#1C578A");
@@ -755,17 +755,25 @@ function showJSPanelTop(panelContainer, targetId, msg, panelSize) {
 }
 
 function showJSPanelRight(panelContainer, targetId, msg, panelSize, x, y) {
+    var element = $('#' + targetId);
+    //var rect = element.getBoundingClientRect();
+    var pos = element.offset();
+    var _x = pos.left + x;
+    var _y = pos.top + y;
     jsPanel.tooltip.create({
-        container: '#divJSContainer',
+        container: '#divJSInfoContainer',
         target: '#' + targetId,
         mode: 'default',
         connector: true,
         position: {
-            my: 'left-top', at: 'right-top', offsetX: x, offsetY: y },
+            my: 'center', at: 'center', offsetX: _x, offsetY: _y
+        },
         theme: 'light filled',
         panelSize: panelSize,
         headerControls: 'closeonly xs',
         headerTitle: 'Description',
+        delay: 0,
         content: msg
     });
 }
+
