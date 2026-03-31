c26e9f30f32cbc5c150112c70a230e870351be0e
FarazAliKhan <Frz.khan1@gmail.com>
2026-03-30 20:43:40 -0400

    Reports method changed

--- WebRazor/Pages/Report.cshtml.cs
+++ WebRazor/Pages/Report.cshtml.cs
@@ -9,8 +9,15 @@ namespace WebRazor.Pages
     [Breadcrumb("ViewData.Report", FromPage = typeof(SelectModel))]
     public class ReportModel : PageModel
     {
-        public void OnGet()
+        public string EmbedUrl { get; set; }
+        public string EmbedToken { get; set; }
+        public string ReportId { get; set; }
+
+        public async Task OnGet()
         {
+            EmbedUrl = "https://app.powerbi.com/view?r=eyJrIjoiMmRiNDgxZDUtNDc0NS00OTQ3LTliYTktN2Y5MTk4M2UwNGRjIiwidCI6IjY0OTM3ZDA0LWI4MDYtNDhiMy04N2U3LWIzNGQyYzBkYWVjMiJ9";
+            EmbedToken = "eyJrIjoiMmRiNDgxZDUtNDc0NS00OTQ3LTliYTktN2Y5MTk4M2UwNGRjIiwidCI6IjY0OTM3ZDA0LWI4MDYtNDhiMy04N2U3LWIzNGQyYzBkYWVjMiJ9";
+            ReportId = "072023c3-4e04-44ae-abf4-507d225315d2";
         }
     }
 }
