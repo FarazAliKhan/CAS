a0d538415e912793aa2cb9f9dc2ea28b363b40ed
InvisorFaraz <frz.khan1@gmail.com>
2026-03-30 21:02:26 -0400

    Report Auth

--- WebRazor/Pages/Report.cshtml.cs
+++ WebRazor/Pages/Report.cshtml.cs
@@ -13,7 +13,7 @@ namespace WebRazor.Pages
         public string EmbedToken { get; set; }
         public string ReportId { get; set; }
 
-        public async Task OnGet()
+public async Task OnGet()
         {
             EmbedUrl = "https://app.powerbi.com/view?r=eyJrIjoiMmRiNDgxZDUtNDc0NS00OTQ3LTliYTktN2Y5MTk4M2UwNGRjIiwidCI6IjY0OTM3ZDA0LWI4MDYtNDhiMy04N2U3LWIzNGQyYzBkYWVjMiJ9";
             EmbedToken = "eyJrIjoiMmRiNDgxZDUtNDc0NS00OTQ3LTliYTktN2Y5MTk4M2UwNGRjIiwidCI6IjY0OTM3ZDA0LWI4MDYtNDhiMy04N2U3LWIzNGQyYzBkYWVjMiJ9";
